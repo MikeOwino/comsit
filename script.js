25865c42e39ebf4cbe1bb3936fbd9770d934e7a3
mikey <mike.owino40@yahoo.com>
2022-05-19 12:25:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const playerInstance = jwplayer("player").setup({
       sources: [
         {
           file:
-            "https://rs1.seedr.cc/ff_get/1165215399/the.simpsons.s33e21.1080p.web.h264-cakes.mkv?st=k27NaGV8MW6ho7aGKAvgsw&e=1652770158",
+            "https://www-9ob48388.ssl0d.com/0O8X_JIOl3UWAuGtu6p7bg/1652980598/I5EXWkZmjUyMzZG-tzFHnppt_6PXGnyOAVi3tdctaR3biFq9MjSS_qNXzZtx0onEaAVJiXtCRuNRPMDttkbEdmpwjuNTZC27bivYXehbY2fKP6Wj0kLkcd7ToXX9l-FNLEKIEQumd15XRdGfQ65D591dancBeaZL6k4akABqYDE2FLzYlO6eZ-tj8RDGU9oElohT8HDZWYIz17m9Z4zN0OJLFFxosaOM9Ktuv5l7Mm0rESajZp64ELP9sUUcKVTOHReciV85iNypPxxX_AuBSvxkx3ci66LD8RHitt_Hl7I/8295151.mp4?dl=1",
           label: "1080p",
           default: true
         },
