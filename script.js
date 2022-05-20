6ec4e2da565f6ed60d57c7881c1a52bf728676c5
mikey <mike.owino40@yahoo.com>
2022-05-20 20:31:09 +0300

    ....

--- script.js
+++ script.js
@@ -42,11 +42,11 @@ const playerInstance = jwplayer("player").setup({
     {
       title: "Planes",
       description: "Now Playing",
-      image: "https://i.ytimg.com/vi/SIJnzoUvR_k/maxresdefault.jpg",
+      image: "https://res.cloudinary.com/weknow-creators/image/upload/v1653067188/sonic-2-poster-game_awkira.jpg",
       sources: [
         {
           file:
-            "https://www-9ob48388.ssl0d.com/0O8X_JIOl3UWAuGtu6p7bg/1652980598/I5EXWkZmjUyMzZG-tzFHnppt_6PXGnyOAVi3tdctaR3biFq9MjSS_qNXzZtx0onEaAVJiXtCRuNRPMDttkbEdmpwjuNTZC27bivYXehbY2fKP6Wj0kLkcd7ToXX9l-FNLEKIEQumd15XRdGfQ65D591dancBeaZL6k4akABqYDE2FLzYlO6eZ-tj8RDGU9oElohT8HDZWYIz17m9Z4zN0OJLFFxosaOM9Ktuv5l7Mm0rESajZp64ELP9sUUcKVTOHReciV85iNypPxxX_AuBSvxkx3ci66LD8RHitt_Hl7I/8295151.mp4?dl=1",
+            "https://nw7.seedr.cc/ff_get/1168023601/Sonic.the.Hedgehog.2.2022.1080p.WEBRip.1400MB.DD2.0.x264-GalaxyRG.mkv?st=EMQqLvuDzNcZNu3MTSpXpw&e=1653154228",
           label: "1080p",
           default: true
         },
